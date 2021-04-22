44a679f20899ab58f2028857410b011fbeb4b831
robert611 <cyrkiel86@interia.pl>
2021-04-22 19:58:03 +0200

    Create repository class with methods: find, findAll, findOneBy, findBy

---
+++ src/Model/Database/Repository/AbstractRepository.php
@@ -0,0 +1,113 @@
+<?php 
+
+namespace App\Model\Database\Repository;
+
+use App\Model\Database\Connection;
+
+abstract class AbstractRepository
+{
+    use Connection;
+    
+    private \PDO $pdo;
+
+    public function __construct()
+    {
+        $this->pdo = $this->getPdoConnection();
+    }
+
+    /*
+    * Contains name of the table associated with repository
+    */
+    public string $table;
+
+    /*
+    * Fetches row from table with given id
+    */
+    public function find(int $id)
+    {
+        try {
+            $dbh = $this->pdo->prepare("SELECT * FROM " . $this->table . " WHERE id = :id");
+            $dbh->execute(['id' => $id]);
+        }
+        catch (\PDOException $e)
+        {
+            echo $e->getMessage();
+        }
+
+        return $dbh->fetch(\PDO::FETCH_ASSOC);
+    }
+
+    /*
+    * Fetches all rows from table
+    */
+    public function findAll(): ?array
+    {
+        try {
+            $dbh = $this->pdo->prepare("SELECT * FROM " . $this->table);
+            $dbh->execute();
+        }
+        catch (\PDOException $e)
+        {
+            echo $e->getMessage();
+        }
+
+        return $dbh->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function findByQuery(array $conditions): object
+    {
+        $query = "SELECT * FROM " . $this->table . "";
+
+        if (!empty($conditions))
+        {
+            $query .= " WHERE ";
+        } 
+
+        $iteration = 1;
+
+        foreach ($conditions as $column => $condition)
+        {
+            if ($iteration > 1)
+            {
+                $query .= " AND " . $column . " = :" . $column;
+            }
+            else 
+            {
+                $query .= $column . " = :" . $column;
+            }
+
+            $iteration++;
+        }
+
+        try {
+            $dbh = $this->pdo->prepare($query);
+            $dbh->execute($conditions);
+        }  
+        catch (\PDOException $e)
+        {
+            echo $e->getMessage();
+        }
+
+        return $dbh;
+    }
+
+    /*
+    * Fetch a record which matches given conditions
+    */
+    public function findOneBy(array $conditions)
+    {
+        $dbh = $this->findByQuery($conditions);
+
+        return $dbh->fetch(\PDO::FETCH_ASSOC);
+    }
+
+    /*
+    * Fetch all records which matches given conditions
+    */
+    public function findBy(array $conditions): ?array
+    {
+        $dbh = $this->findByQuery($conditions);
+
+        return $dbh->fetchAll(\PDO::FETCH_ASSOC);
+    }
+}
