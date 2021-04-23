d64a6d98506ad6c19291c5ca4035c2a82af31bd1
robert611 <cyrkiel86@interia.pl>
2021-04-23 18:39:07 +0200

    Add symfony http-foundation component

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -2,16 +2,25 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+
 abstract class AbstractController
 {
     protected \Twig\Loader\FilesystemLoader $twigLoader;
     protected \Twig\Environment $twig;
+    protected Request $request;
+    protected Session $session;
 
     public function __construct()
     {
+        $this->request = Request::createFromGlobals();
+        $this->session = new Session();
+        $this->session->start();
+
         $this->loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/../../templates');
         $this->twig = new \Twig\Environment($this->loader, ['debug' => true]);  
         $this->twig->addExtension(new \Twig\Extension\DebugExtension());
-        $this->twig->addGlobal('session', $_SESSION);
+        $this->twig->addGlobal('session', $this->session);
     }
 }
