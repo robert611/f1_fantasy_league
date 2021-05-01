23d6bc6b5d2df252f9761872b312594c864b800b
robert611 <cyrkiel86@interia.pl>
2021-05-01 18:29:49 +0200

    Add raw_password index to users records in user fixtures

--- src/Model/Database/Fixtures/UserFixtures.php
+++ src/Model/Database/Fixtures/UserFixtures.php
@@ -17,11 +17,11 @@ class UserFixtures extends Fixture implements FixturesInterface
     public function getUserRecords(): array
     {
         return [
-            ['username' => 'tomy1242', 'email' => 'test1242@email.com', 'roles' => '["ROLE_USER"]', 'password' => 'tomy1242'],
-            ['username' => 'maria1942', 'email' => 'maria1942@email.com', 'roles' => '["ROLE_USER"]', 'password' => 'maria1942'],
-            ['username' => 'michael11', 'email' => 'michael11@email.com', 'roles' => '["ROLE_USER"]', 'password' => 'michael11'],
-            ['username' => 'veronica935', 'email' => 'veronica935@email.com', 'roles' => '["ROLE_USER"]', 'password' => 'veronica935'],
-            ['username' => 'john9022', 'email' => 'john902@email.com', 'roles' => '["ROLE_USER"]', 'password' => 'john902']
+            ['username' => 'tomy1242', 'email' => 'test1242@email.com', 'roles' => '["ROLE_USER"]', 'raw_password' => 'tomy1242', 'password' => password_hash('tomy1242', PASSWORD_BCRYPT)],
+            ['username' => 'maria1942', 'email' => 'maria1942@email.com', 'roles' => '["ROLE_USER"]', 'raw_password' => 'maria1942', 'password' => password_hash('maria1942', PASSWORD_BCRYPT)],
+            ['username' => 'michael11', 'email' => 'michael11@email.com', 'roles' => '["ROLE_USER"]', 'raw_password' => 'michael11','password' => password_hash('michael11', PASSWORD_BCRYPT)],
+            ['username' => 'veronica935', 'email' => 'veronica935@email.com', 'roles' => '["ROLE_USER"]', 'raw_password' => 'veronica935','password' => password_hash('veronica935', PASSWORD_BCRYPT)],
+            ['username' => 'john9022', 'email' => 'john902@email.com', 'roles' => '["ROLE_USER"]', 'raw_password' => 'john902', 'password' => password_hash('john902', PASSWORD_BCRYPT)]
         ];
     }
 
@@ -33,6 +33,7 @@ class UserFixtures extends Fixture implements FixturesInterface
 
         foreach ($userRecords as $record)
         {
+            unset($record['raw_password']);
             $this->queryBuilder->executeQuery("INSERT INTO user VALUES (null, :username, :email, :roles, :password)", $record);
         }
     }
