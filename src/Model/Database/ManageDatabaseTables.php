1d89912307500899d556d5a2caefbb447bd53c82
robert611 <cyrkiel86@interia.pl>
2021-05-14 19:19:34 +0200

    Change name of team column to team_id in driver table

--- src/Model/Database/ManageDatabaseTables.php
+++ src/Model/Database/ManageDatabaseTables.php
@@ -18,7 +18,7 @@ class ManageDatabaseTables
         return [
             'user' => 'CREATE TABLE user (id INT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(180) unique NOT NULL, email VARCHAR(180) unique NOT NULL, roles longtext DEFAULT NULL, password VARCHAR(2048) NOT NULL)',
             'team' => 'CREATE TABLE team (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(128), picture VARCHAR(128))',
-            'driver' => 'CREATE TABLE driver (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(128), driver_number smallint, team int)',
+            'driver' => 'CREATE TABLE driver (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(128), driver_number smallint, team_id int)',
             'race' => 'CREATE TABLE race (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(128), race_start text)',
             'login' => 'CREATE TABLE login (id INT PRIMARY KEY AUTO_INCREMENT, user_id INT, date VARCHAR(32))'
         ];
@@ -27,7 +27,7 @@ class ManageDatabaseTables
     public function getQueriesCreatingForeignKeys()
     {
         return [
-            'ALTER TABLE driver ADD FOREIGN KEY (team) REFERENCES team(id)',
+            'ALTER TABLE driver ADD FOREIGN KEY (team_id) REFERENCES team(id)',
             'ALTER TABLE login ADD FOREIGN KEY (user_id) REFERENCES user(id)'
         ];
     }
