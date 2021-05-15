8d4a4e51aed4f0cd767fd91a5ca4e9b5eb5226fd
robert611 <cyrkiel86@interia.pl>
2021-05-15 17:53:32 +0200

    Add surname column to driver table

--- src/Model/Database/ManageDatabaseTables.php
+++ src/Model/Database/ManageDatabaseTables.php
@@ -18,7 +18,7 @@ class ManageDatabaseTables
         return [
             'user' => 'CREATE TABLE user (id INT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(180) unique NOT NULL, email VARCHAR(180) unique NOT NULL, roles longtext DEFAULT NULL, password VARCHAR(2048) NOT NULL)',
             'team' => 'CREATE TABLE team (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(128), picture VARCHAR(128))',
-            'driver' => 'CREATE TABLE driver (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(128), driver_number smallint, team_id int)',
+            'driver' => 'CREATE TABLE driver (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(128), surname VARCHAR(128), driver_number smallint, team_id int)',
             'race' => 'CREATE TABLE race (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(128), race_start text)',
             'login' => 'CREATE TABLE login (id INT PRIMARY KEY AUTO_INCREMENT, user_id INT, date VARCHAR(32))'
         ];
