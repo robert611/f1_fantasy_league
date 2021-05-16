d15245b2a71de61e0dbe69855effd16bcf259757
robert611 <cyrkiel86@interia.pl>
2021-05-16 16:57:06 +0200

    Add tables with races and drivers in the homepage template

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,12 +2,30 @@
 
 namespace App\Controller;
 
+use App\Model\Database\Repository\DriverRepository;
+use App\Model\Database\Repository\RaceRepository;
+
 class IndexController extends AbstractController
 {
+    private DriverRepository $driverRepository;
+    private RaceRepository $raceRepository;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->driverRepository = new DriverRepository();
+        $this->raceRepository = new RaceRepository();
+    }
+
     public function home()
     {
         $this->denyAccessUnlessGranted('ROLE_USER');
 
-        print $this->twig->render('home.html.twig');
+        $drivers = $this->driverRepository->findAllWithTeams();
+        $races = $this->raceRepository->findAll();
+
+
+        print $this->twig->render('home.html.twig', ['drivers' => $drivers, 'races' => $races]);
     }
 }
--- src/Model/Database/Repository/DriverRepository.php
+++ src/Model/Database/Repository/DriverRepository.php
@@ -5,4 +5,11 @@ namespace App\Model\Database\Repository;
 class DriverRepository extends AbstractRepository
 {
     public string $table = 'driver';
+
+    public function findAllWithTeams(): array
+    {
+        $query = "SELECT driver.id, driver.name, driver.surname, driver.driver_number, driver.team_id, team.name as team_name, team.picture as team_picture FROM driver, team WHERE driver.team_id = team.id";
+
+        return $this->queryBuilder->queryWithFetchAll($query);
+    }
 }
