35f32ec0b7ffad8e6f080a2022140f8e58bbfa3b
robert611 <cyrkiel86@interia.pl>
2021-05-19 18:49:48 +0200

    Add id field to user object

--- src/Model/Auth/User.php
+++ src/Model/Auth/User.php
@@ -4,12 +4,14 @@ namespace App\Model\Auth;
 
 class User 
 {
+    private int $id;
     private string $username;
     private string $email;
     private array $roles;
 
     public function __construct(array $userData)
     {
+        $this->id = $userData['id'];
         $this->username = $userData['username'];
         $this->email = $userData['email'];
 
@@ -19,6 +21,11 @@ class User
     public function hasRole(string $role): bool
     {
         return in_array($role, $this->getRoles());
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
     }
 
     public function getUsername(): string
