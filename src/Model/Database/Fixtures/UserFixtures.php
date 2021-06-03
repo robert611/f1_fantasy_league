4f7a2e15cc0788cc0227be9359cff4847da76ba1
robert611 <cyrkiel86@interia.pl>
2021-06-03 16:59:33 +0200

    Add user with admin role to fixtures

--- src/Model/Database/Fixtures/UserFixtures.php
+++ src/Model/Database/Fixtures/UserFixtures.php
@@ -9,7 +9,7 @@ class UserFixtures extends Fixture implements FixturesInterface
     public function getRecords(): array
     {
         return [
-            ['username' => 'tomy1242', 'email' => 'test1242@email.com', 'roles' => '["ROLE_USER"]', 'raw_password' => 'tomy1242', 'password' => password_hash('tomy1242', PASSWORD_BCRYPT)],
+            ['username' => 'tomy1242', 'email' => 'test1242@email.com', 'roles' => '["ROLE_USER", "ROLE_ADMIN"]', 'raw_password' => 'tomy1242', 'password' => password_hash('tomy1242', PASSWORD_BCRYPT)],
             ['username' => 'maria1942', 'email' => 'maria1942@email.com', 'roles' => '["ROLE_USER"]', 'raw_password' => 'maria1942', 'password' => password_hash('maria1942', PASSWORD_BCRYPT)],
             ['username' => 'michael11', 'email' => 'michael11@email.com', 'roles' => '["ROLE_USER"]', 'raw_password' => 'michael11','password' => password_hash('michael11', PASSWORD_BCRYPT)],
             ['username' => 'veronica935', 'email' => 'veronica935@email.com', 'roles' => '["ROLE_USER"]', 'raw_password' => 'veronica935','password' => password_hash('veronica935', PASSWORD_BCRYPT)],
