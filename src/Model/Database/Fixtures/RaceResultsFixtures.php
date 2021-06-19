c4ce300cdae78b6c1d5caddc9b1c4ffaf4f44f13
robert611 <cyrkiel86@interia.pl>
2021-06-19 19:56:12 +0200

    Add race results fixture for two more races

--- src/Model/Database/Fixtures/RaceResultsFixtures.php
+++ src/Model/Database/Fixtures/RaceResultsFixtures.php
@@ -12,26 +12,72 @@ class RaceResultsFixtures extends Fixture implements FixturesInterface
         $driversIds = (new DriverRepository())->findDriversIds();
 
         return [
-            ['race_id' => 1, 'driver_id' => $driversIds[8], 'position' => 1],
-            ['race_id' => 1, 'driver_id' => $driversIds[13], 'position' => 2],
-            ['race_id' => 1, 'driver_id' => $driversIds[9], 'position' => 3],
-            ['race_id' => 1, 'driver_id' => $driversIds[7], 'position' => 4],
-            ['race_id' => 1, 'driver_id' => $driversIds[12], 'position' => 5],
-            ['race_id' => 1, 'driver_id' => $driversIds[0], 'position' => 6],
-            ['race_id' => 1, 'driver_id' => $driversIds[6], 'position' => 7],
-            ['race_id' => 1, 'driver_id' => $driversIds[1], 'position' => 8],
-            ['race_id' => 1, 'driver_id' => $driversIds[16], 'position' => 9],
-            ['race_id' => 1, 'driver_id' => $driversIds[11], 'position' => 10],
-            ['race_id' => 1, 'driver_id' => $driversIds[2], 'position' => 11],
-            ['race_id' => 1, 'driver_id' => $driversIds[3], 'position' => 12],
-            ['race_id' => 1, 'driver_id' => $driversIds[15], 'position' => 13],
-            ['race_id' => 1, 'driver_id' => $driversIds[18], 'position' => 14],
-            ['race_id' => 1, 'driver_id' => $driversIds[10], 'position' => 15],
-            ['race_id' => 1, 'driver_id' => $driversIds[4], 'position' => 16],
-            ['race_id' => 1, 'driver_id' => $driversIds[17], 'position' => 17],
-            ['race_id' => 1, 'driver_id' => $driversIds[19], 'position' => 18],
-            ['race_id' => 1, 'driver_id' => $driversIds[14], 'position' => 0],
-            ['race_id' => 1, 'driver_id' => $driversIds[5], 'position' => 0]
+            1 => [
+                ['driver_id' => $driversIds[8], 'position' => 1],
+                ['driver_id' => $driversIds[13], 'position' => 2],
+                ['driver_id' => $driversIds[9], 'position' => 3],
+                ['driver_id' => $driversIds[7], 'position' => 4],
+                ['driver_id' => $driversIds[12], 'position' => 5],
+                ['driver_id' => $driversIds[0], 'position' => 6],
+                ['driver_id' => $driversIds[6], 'position' => 7],
+                ['driver_id' => $driversIds[1], 'position' => 8],
+                ['driver_id' => $driversIds[16], 'position' => 9],
+                ['driver_id' => $driversIds[11], 'position' => 10],
+                ['driver_id' => $driversIds[2], 'position' => 11],
+                ['driver_id' => $driversIds[3], 'position' => 12],
+                ['driver_id' => $driversIds[15], 'position' => 13],
+                ['driver_id' => $driversIds[18], 'position' => 14],
+                ['driver_id' => $driversIds[10], 'position' => 15],
+                ['driver_id' => $driversIds[4], 'position' => 16],
+                ['driver_id' => $driversIds[17], 'position' => 17],
+                ['driver_id' => $driversIds[19], 'position' => 18],
+                ['driver_id' => $driversIds[14], 'position' => 0],
+                ['driver_id' => $driversIds[5], 'position' => 0]
+            ],
+            2 => [
+                ['driver_id' => $driversIds[13], 'position' => 1],
+                ['driver_id' => $driversIds[8], 'position' => 2],
+                ['driver_id' => $driversIds[7], 'position' => 3],
+                ['driver_id' => $driversIds[0], 'position' => 4],
+                ['driver_id' => $driversIds[1], 'position' => 5],
+                ['driver_id' => $driversIds[6], 'position' => 6],
+                ['driver_id' => $driversIds[17], 'position' => 7],
+                ['driver_id' => $driversIds[11], 'position' => 8],
+                ['driver_id' => $driversIds[15], 'position' => 9],
+                ['driver_id' => $driversIds[14], 'position' => 10],
+                ['driver_id' => $driversIds[12], 'position' => 11],
+                ['driver_id' => $driversIds[16], 'position' => 12],
+                ['driver_id' => $driversIds[2], 'position' => 13],
+                ['driver_id' => $driversIds[3], 'position' => 14],
+                ['driver_id' => $driversIds[10], 'position' => 15],
+                ['driver_id' => $driversIds[4], 'position' => 16],
+                ['driver_id' => $driversIds[5], 'position' => 17],
+                ['driver_id' => $driversIds[9], 'position' => 0],
+                ['driver_id' => $driversIds[18], 'position' => 0],
+                ['driver_id' => $driversIds[19], 'position' => 0]
+            ],
+            3 => [
+                ['driver_id' => $driversIds[8], 'position' => 1],
+                ['driver_id' => $driversIds[13], 'position' => 2],
+                ['driver_id' => $driversIds[9], 'position' => 3],
+                ['driver_id' => $driversIds[12], 'position' => 4],
+                ['driver_id' => $driversIds[7], 'position' => 5],
+                ['driver_id' => $driversIds[0], 'position' => 6],
+                ['driver_id' => $driversIds[15], 'position' => 7],
+                ['driver_id' => $driversIds[14], 'position' => 8],
+                ['driver_id' => $driversIds[6], 'position' => 9],
+                ['driver_id' => $driversIds[17], 'position' => 10],
+                ['driver_id' => $driversIds[1], 'position' => 11],
+                ['driver_id' => $driversIds[3], 'position' => 12],
+                ['driver_id' => $driversIds[10], 'position' => 13],
+                ['driver_id' => $driversIds[11], 'position' => 14],
+                ['driver_id' => $driversIds[16], 'position' => 15],
+                ['driver_id' => $driversIds[18], 'position' => 16],
+                ['driver_id' => $driversIds[4], 'position' => 17],
+                ['driver_id' => $driversIds[19], 'position' => 18],
+                ['driver_id' => $driversIds[5], 'position' => 19],
+                ['driver_id' => $driversIds[2], 'position' => 0]
+            ]
         ];
     }
 
@@ -39,9 +85,13 @@ class RaceResultsFixtures extends Fixture implements FixturesInterface
     {
         $raceResultsRecords = $this->getRecords();
 
-        foreach ($raceResultsRecords as $record)
+        foreach ($raceResultsRecords as $raceId => $raceResults)
         {
-            $this->queryBuilder->executeQuery("INSERT INTO race_results VALUES (null, :race_id, :driver_id, :position)", $record);
+            foreach ($raceResults as $result)
+            {
+                $this->queryBuilder->executeQuery("INSERT INTO race_results VALUES (null, :race_id, :driver_id, :position)", 
+                ['race_id' => $raceId, 'driver_id' => $result['driver_id'], 'position' => $result['position']]);
+            }
         }
     }
 
