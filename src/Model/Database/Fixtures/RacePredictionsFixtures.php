ff3c7bc537cb874d24fbc52deb31fad1bc42b289
robert611 <cyrkiel86@interia.pl>
2021-06-20 19:03:58 +0200

    Add more RacePredictions fixtures

--- src/Model/Database/Fixtures/RacePredictionsFixtures.php
+++ src/Model/Database/Fixtures/RacePredictionsFixtures.php
@@ -33,6 +33,27 @@ class RacePredictionsFixtures extends Fixture implements FixturesInterface
             ['race_id' => 1, 'user_id' => $userId, 'driver_id' => $driversIds[13], 'position' => 18],
             ['race_id' => 1, 'user_id' => $userId, 'driver_id' => $driversIds[14], 'position' => 19],
             ['race_id' => 1, 'user_id' => $userId, 'driver_id' => $driversIds[15], 'position' => 20],
+
+            ['race_id' => 2, 'user_id' => $userId, 'driver_id' => $driversIds[2], 'position' => 1],
+            ['race_id' => 2, 'user_id' => $userId, 'driver_id' => $driversIds[3], 'position' => 2],
+            ['race_id' => 2, 'user_id' => $userId, 'driver_id' => $driversIds[11], 'position' => 3],
+            ['race_id' => 2, 'user_id' => $userId, 'driver_id' => $driversIds[1], 'position' => 4],
+            ['race_id' => 2, 'user_id' => $userId, 'driver_id' => $driversIds[16], 'position' => 5],
+            ['race_id' => 2, 'user_id' => $userId, 'driver_id' => $driversIds[0], 'position' => 6],
+            ['race_id' => 2, 'user_id' => $userId, 'driver_id' => $driversIds[8], 'position' => 7],
+            ['race_id' => 2, 'user_id' => $userId, 'driver_id' => $driversIds[7], 'position' => 8],
+            ['race_id' => 2, 'user_id' => $userId, 'driver_id' => $driversIds[19], 'position' => 9],
+            ['race_id' => 2, 'user_id' => $userId, 'driver_id' => $driversIds[14], 'position' => 10],
+            ['race_id' => 2, 'user_id' => $userId, 'driver_id' => $driversIds[12], 'position' => 11],
+            ['race_id' => 2, 'user_id' => $userId, 'driver_id' => $driversIds[17], 'position' => 12],
+            ['race_id' => 2, 'user_id' => $userId, 'driver_id' => $driversIds[13], 'position' => 13],
+            ['race_id' => 2, 'user_id' => $userId, 'driver_id' => $driversIds[6], 'position' => 14],
+            ['race_id' => 2, 'user_id' => $userId, 'driver_id' => $driversIds[10], 'position' => 15],
+            ['race_id' => 2, 'user_id' => $userId, 'driver_id' => $driversIds[18], 'position' => 16],
+            ['race_id' => 2, 'user_id' => $userId, 'driver_id' => $driversIds[5], 'position' => 17],
+            ['race_id' => 2, 'user_id' => $userId, 'driver_id' => $driversIds[4], 'position' => 18],
+            ['race_id' => 2, 'user_id' => $userId, 'driver_id' => $driversIds[9], 'position' => 19],
+            ['race_id' => 2, 'user_id' => $userId, 'driver_id' => $driversIds[15], 'position' => 20],
         ];
     }
 
